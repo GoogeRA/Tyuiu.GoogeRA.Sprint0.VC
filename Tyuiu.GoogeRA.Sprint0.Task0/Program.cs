37de6278f08c6a316718fe17d5e9da35d0b64c43
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-09-25 14:40:11 +0300

    Add project files.

---
+++ Tyuiu.GoogeRA.Sprint0.Task0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GoogeRA.Sprint0.Task0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
