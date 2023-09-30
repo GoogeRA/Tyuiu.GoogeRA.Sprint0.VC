6c62d0ea10e438e62612057fd3880e5cfe9c789e
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-09-30 15:55:45 +0300

    Task1

---
+++ Tyuiu.GoogeRA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GoogeRA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Роберт";
+            int age = 21;
+            Console.WriteLine(" Здравствуйте, меня зовут " + name + " мне " + age + " год! ");
+            Console.ReadKey();
+        }
+    }
+}
